5a3a724dbc4ef267b8fc9a0dfd4747f98ea054d9
RaviJangidPIO <Ravi.Jangid@programmers.io>
2024-09-09 11:54:06 +0530

    first commit

---
+++ src/main/java/com/example/springbootravi/jangid/springBootPr/SpringBootPrApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootravi.jangid.springBootPr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootravi/jangid/springBootPr/SpringBootPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootravi.jangid.springBootPr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
